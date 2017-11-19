5739c4b8c601f3fbc73b2daf2a0bae1698931e28
paubin75 <33636079+paubin75@users.noreply.github.com>
2017-11-19 20:05:52 +0100

    version server
    serveur nodejs/express

---
+++ serveur/interrupteur.js
@@ -0,0 +1,28 @@
+var WeMo = require('wemo');
+var express = require('express');
+var router = express.Router();
+
+router.get('/:piece/:etat', function(req, res, next) {
+	var piece=req.params.piece;
+	var etat;
+	if (req.params.etat=="on"){
+	  etat=1;
+	}
+	if (req.params.etat=="off"){
+	  etat=0;
+	}
+
+	WeMo.Search(piece, function(err, device) {
+		var wemoSwitch = new WeMo(device.ip, device.port);
+	  wemoSwitch.setBinaryState(etat, function(err, result) {
+	  	if (err) console.error(err);
+		  console.log(result);
+	  	wemoSwitch.getBinaryState(function(err, result) {
+	    		if (err) console.error(err);
+	    		console.log(result);
+		   });
+	   });
+	});
+});
+
+module.exports = router;
